a70c81d1619df2713b7f8d3e4c595d13502296de
AntoinePassemiers <apassemi@ulb.ac.be>
2019-04-07 03:50:20 +0200

    Update sets.json

--- data/trim_alignments.py
+++ data/trim_alignments.py
@@ -40,11 +40,13 @@ def trim(alignment):
         alignment[i] = alignment[i][:L]
 
 
-for dp, dn, filenames in os.walk('.'):
-    for f in filenames:
-        if f == 'alignment.a3m':
-            print(os.path.join(dp, f))
-            data = parse_fasta(os.path.join(dp, f))
-            trim(data['sequences'])
-            assert(len(data['sequences']) == len(data['comments']))
-            write_fasta(os.path.join(dp, f), data)
+
+if __name__ == '__main__':
+    for dp, dn, filenames in os.walk('.'):
+        for f in filenames:
+            if f == 'alignment.a3m':
+                print(os.path.join(dp, f))
+                data = parse_fasta(os.path.join(dp, f))
+                trim(data['sequences'])
+                assert(len(data['sequences']) == len(data['comments']))
+                write_fasta(os.path.join(dp, f), data)
